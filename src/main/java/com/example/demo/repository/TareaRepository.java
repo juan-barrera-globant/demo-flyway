8e27a64504f69386fc8fa42f166da07f39a27315
juan-barrera-globant <juan.barrera@globant.com>
2026-04-08 14:00:53 -0500

    delete

--- src/main/java/com/example/demo/repository/TareaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "tareas", path = "tareas")
-public interface TareaRepository extends JpaRepository<Tarea, Long> {
-}
--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tarea
-(
-    id          BIGSERIAL PRIMARY KEY,
-    titulo      VARCHAR(255) NOT NULL,
-    descripcion TEXT
-);
-
